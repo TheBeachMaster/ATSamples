eff6cd4bcbd358b8f506383a1c28b0a2e869afaf
TheBeachmaster <otienoken@outlook.com>
2017-11-28 19:05:39 +0300

    Updated Config

--- payments.node/config.js
+++ payments.node/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    apikey: "",
+    apikey: "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745",
     username: "sandbox",
-    product_name: "",
+    product_name: "coolproduct",
     currency: "KES",
     environment: true //Sandbox
 }
