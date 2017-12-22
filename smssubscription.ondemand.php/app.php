2ea25ae530028efd8c1a6c5b67da9630ea5755c9
TheBeachmaster <otienoken@outlook.com>
2017-12-22 14:09:27 +0300

    Subscription sample :TBD

---
+++ smssubscription.ondemand.php/app.php
@@ -0,0 +1,31 @@
+<?php
+    require_once('libs/AfricasTalkingGateway.php');
+    require_once('conf/accountconfig.php');
+    $from = $_POST['from'];
+    $to = $_POST['to'];
+    $text = $_POST['text'];
+    $date = $_POST['date'];
+    $id = $_POST['id'];
+    $linkId = $_POST['linkId'];
+    $recipient = "+254724587654";
+    $shortCode = "44005";
+    $keyword   = "coolguy";
+    $bulkSMSMode = 0;
+    $options = array('keyword'=> $keyword,'linkId'=> $linkId, 'retryDurationInHours' => "12");
+    $message = "Get your daily message and thats how we roll.";
+    $gateway    = new AfricasTalkingGateway($username, $apikey, $env);
+    try 
+    { 
+    $results = $gateway->sendMessage($recipient, $message, $shortCode, $bulkSMSMode, $options);
+    foreach($results as $result) {
+        echo " Number: " .$result->number;
+        echo " Status: " .$result->status;
+        echo " MessageId: " .$result->messageId . "\n";
+                                 }
+    }
+    catch ( AfricasTalkingGatewayException $e )
+    {
+    echo "Encountered an error while sending: ".$e->getMessage();
+    }
+
+?>
---
+++ smssubscription.ondemand.php/conf/accountconfig.php
@@ -0,0 +1,5 @@
+<?php
+$username   = "sandbox";
+$apikey     = "afd635a4f295dd936312836c0b944d55f2a836e8ff2b63987da5e717cd5ff745";
+$env = "sandbox";
+?>
