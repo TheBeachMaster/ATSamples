737018ffe93fbc1e84a993fbcb6d788fdc416550
TheBeachmaster <otienoken@outlook.com>
2018-02-20 22:15:04 +0300

    Add gitingnore

--- ussdregistration.php/conf/dbconfig.php
+++ ussdregistration.php/conf/dbconfig.php
@@ -1,12 +1,12 @@
 <?php 
 $servername = 'localhost';
 $username = 'root';
-$password = "Infrared@1944";
+$password = "";
 $database = "atussd";
 $dbport = 3306;
     $db = new mysqli($servername, $username, $password, $database, $dbport);
     if ($db->connect_error) {
         header('Content-type: text/plain');
-        die("END An error was encountered. Please try again later");
+        die("An error was encountered. Please try again later");
     } 
 ?>
