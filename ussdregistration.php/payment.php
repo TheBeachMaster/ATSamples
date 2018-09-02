8386b3760f09319688458a1cbe419b56bbce2f3c
TheBeachmaster <otienoken@outlook.com>
2018-09-02 10:51:38 +0300

    Fix incorrect params

--- ussdregistration.php/payment.php
+++ ussdregistration.php/payment.php
@@ -1,9 +1,13 @@
 <?php
 if(!empty($_POST)){
-    $sessionId=$_POST['status'];
-	$serviceCode=$_POST['description'];
-    $phoneNumber=$_POST['transactionId'];
-
+    $transactionId = $_POST['transactionId'];
+	$paymentCode = $_POST['providerRefId'];
+    $paymentSource = $_POST['source'];
+    $paymentStatus = $_POST['status'];
+    $paymentAmount = $_POST['value'];
+    $paymentMetaData = $_POST['requestMetadata'];
+    $providerMetadata = $_POST['providerMetadata'];
+    
     $response = "OK";
     header('Content-type: text/plain');
     echo $response;	
